69031b60f769db25dd5585e7329b7d219518c157
helado de brownie <helado.de.brownie@gmail.com>
2020-06-26 17:34:00 -0400

    turn all messages into templates; separate out player logic better

--- AutomaticBrewer.cs
+++ AutomaticBrewer.cs
@@ -1,7 +1,19 @@
+using static XRL.UI.ConversationUI;
+
 namespace XRL.World.Parts
 {
     public class helado_BrewedBeverages_AutomaticBrewer : IPart
     {
+        public const string MESSAGE_ACTIVATE = "=capitalize==subject.the==subject.name= =verb:activate= =object.the==object.name=.";
+        public const string MESSAGE_BREWING_BEGIN = "=capitalize==subject.the==subject.name= =verb:whine= and =verb:grind= as =pronouns.subjective= =verb:gain:afterpronoun= momentum, then =verb:set= busily to work processing =pronouns.possessive= input.";
+        public const string MESSAGE_BREWING_ABORT = "=capitalize==subject.the==subject.name= =verb:growl= in annoyance and =verb:abort= =pronouns.possessive= processes.";
+        public const string MESSAGE_BREWING_CONTINUE_FINE = "=capitalize==subject.the==subject.name= =verb:hum= contentedly.";
+        public const string MESSAGE_BREWING_CONTINUE_POOR = "=capitalize==subject.the==subject.name= =verb:hum= anxiously.";
+        public const string MESSAGE_BREWING_SUCCESS = "=capitalize==subject.the==subject.name= =verb:chime= a triumphant jingle and dispense =liquid=.";
+        public const string MESSAGE_BREWING_SUCCESS_TRICKY = "=capitalize==subject.the==subject.name= =verb:chime= smugly and =verb:serve= up =liquid=.";
+        public const string MESSAGE_BREWING_FAILURE = "=capitalize==subject.the==subject.name= =verb:break= into a coughing fit and =verb:vomit= up =liquid=.";
+        public const string MESSAGE_REFUSAL_DISH_OCCUPIED = "=subject.The==subject.name= patiently =verb:flash= a pair of lights on either side of =pronouns.possessive= liquid dish.";
+
         byte TurnsLeft = 0;
 
         public void Activate()
@@ -12,25 +24,17 @@ namespace XRL.World.Parts
             {
                 TurnsLeft = 3;
 
-                AddPlayerMessage(
-                    ParentObject.The +
-                    ParentObject.DisplayNameOnly +
-                    " whines and grinds as " +
-                    ParentObject.its +
-                    " mechanisms gain momentum, then sets busily to work processing " +
-                    ParentObject.its +
-                    " input."
-                );
+                AddPlayerMessage(VariableReplace(
+                    MESSAGE_BREWING_BEGIN,
+                    ParentObject
+                ));
             }
             else
             {
-                AddPlayerMessage(
-                    ParentObject.The +
-                    ParentObject.DisplayNameOnly +
-                    " patiently flashes a pair of lights on either side of " +
-                    ParentObject.its +
-                    " liquid dish."
-                );
+                AddPlayerMessage(VariableReplace(
+                    MESSAGE_REFUSAL_DISH_OCCUPIED,
+                    ParentObject
+                ));
             }
         }
 
@@ -50,24 +54,23 @@ namespace XRL.World.Parts
 
                 if (TurnsLeft > 0)
                 {
-                    XDidY(
-                        ParentObject,   // agent
-                        "hum",          // verb
-                        "anxiously"     // part to append
-                    );
+                    AddPlayerMessage(VariableReplace(
+                        MESSAGE_BREWING_CONTINUE_POOR,
+                        ParentObject
+                    ));
                 }
                 else
                 {
                     var liquid = ParentObject.GetPart<LiquidVolume>();
                     liquid.MixWith(new LiquidVolume("putrid", 1));
 
-                    AddPlayerMessage(
-                        ParentObject.The +
-                        ParentObject.DisplayNameOnly +
-                        " breaks into a coughing fit and vomits up " +
-                        liquid.GetLiquidName() +
-                        "."
-                    );
+                    AddPlayerMessage(VariableReplace(
+                        MESSAGE_BREWING_FAILURE.Replace(
+                            "=liquid=",
+                            liquid.GetLiquidName()
+                        ),
+                        ParentObject
+                    ));
                 }
             }
 
@@ -85,10 +88,7 @@ namespace XRL.World.Parts
                 0,
                 false,
                 false,              // does not work at a distance
-                true,               // unless we have telekinesis
-                false,              // but not telepathy
-                true,               // signal this action as a min event
-                null                // fired on the object that was activated
+                true                // unless we have telekinesis
             );
 
             return true;
@@ -96,18 +96,26 @@ namespace XRL.World.Parts
 
         public override bool HandleEvent(InventoryActionEvent E)
         {
+            var message = VariableReplace(
+                MESSAGE_ACTIVATE,           // string to substitute in
+                E.Actor,                    // subject
+                null,
+                false,
+                ParentObject                // object
+            );
+
             if (E.Command == "Activate")
             {
-                XDidYToZ(
-                   E.Actor,             // agent
-                   "activate",          // verb
-                   ParentObject,        // patient
-                   null,
-                   null,
-                   E.Actor.IsPlayer()   // show as dialog if done by the player
-               );
+                if (E.Actor.IsPlayer())
+                {
+                    XRL.UI.Popup.Show(message);
+                    E.RequestInterfaceExit();
+                }
+                else
+                {
+                    AddPlayerMessage(message);
+                }
 
-                E.RequestInterfaceExit();
                 Activate();
                 return true;
             }
