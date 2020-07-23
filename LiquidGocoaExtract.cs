e446b5da74ad3df44aff2c54cc6127ab2dd94867
helado de brownie <helado.de.brownie@gmail.com>
2020-07-23 15:05:32 -0400

    colorize!

--- LiquidGocoaExtract.cs
+++ LiquidGocoaExtract.cs
@@ -1,18 +1,27 @@
+using System.Collections.Generic;
+using XRL.World;
 using XRL.World.Parts;
 
 namespace XRL.Liquids
 {
-    [System.Serializable]
     [IsLiquid]
+    [System.Serializable]
     public class helado_LiquidGocoaExtract : BaseLiquid
     {
         public new const string ID = "gocoaextract";
-        public const string ADJECTIVE = "fast";
+        public const string ADJECTIVE = "{{b-B sequence|fast}}";
+
+        public static List<string> Colors = new List<string>(2)
+        {
+            "B",
+            "b",
+        };
 
         public helado_LiquidGocoaExtract() : base(ID) { }
 
-        public override string GetName(LiquidVolume _) {
-            return "gocoa extract";
+        public override string GetName(LiquidVolume _)
+        {
+            return "{{b-B sequence|gocoa extract}}";
         }
 
         public override string GetAdjective(LiquidVolume _)
@@ -23,6 +32,31 @@ namespace XRL.Liquids
         public override string GetSmearedAdjective(LiquidVolume _)
         {
             return ADJECTIVE;
+        }
+
+        public override string GetSmearedName(LiquidVolume _)
+        {
+            return ADJECTIVE;
+        }
+
+        public override string GetStainedName(LiquidVolume _)
+        {
+            return "{{b-B sequence|gocoa}}";
+        }
+
+        public override string GetColor()
+        {
+            return Colors[0];
+        }
+
+        public override List<string> GetColors()
+        {
+            return Colors;
+        }
+
+        public override float GetValuePerDram()
+        {
+            return 100;
         }
     }
 }
